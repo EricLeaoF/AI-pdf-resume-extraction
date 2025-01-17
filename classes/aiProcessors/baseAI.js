9804e459addfdcff4d82b2f2ff38a2b39c617987
eric.freitas <eric.freitas@icolabora.com>
2025-01-17 11:56:25 -0300

    added GEMINI

--- classes/aiProcessors/baseAI.js
+++ classes/aiProcessors/baseAI.js
@@ -1,11 +1,12 @@
 const fs = require('fs/promises');
 const chatGpt = require('./chatGpt');
+const gemini = require('./gemini');
 
 class BaseAI {
 
   async sendTextAI(resumeText) {
     const prompt = await fs.readFile('./prompt/prompt.txt', 'utf8');
-    return await chatGpt(prompt + resumeText)
+    return process.env.CHATGPT_API_KEY ? await chatGpt(prompt + resumeText) : await gemini(prompt + resumeText);
   }
 }
 
---
+++ classes/aiProcessors/gemini.js
@@ -0,0 +1,20 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+module.exports = (async (content) => { 
+
+  console.log('Starting GEMINI AI Process...')
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  const result = await model.generateContent(content);
+  const response = await result.response;
+
+  function extractJSON(text) {
+    const prefixRegex = /^(JSON| JSON|json| json)\s*/;
+    const inner = text.replace(/```(.*?)```/s, '$1');
+    return inner.replace(prefixRegex, ''); 
+  }
+
+  const text = extractJSON(response.text());
+  console.log('...Finished AI Process!')
+  return JSON.parse(text);
+})
